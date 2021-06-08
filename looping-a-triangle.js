796b1aa73596b484ed1c966a0e8c6f8fe676fd74
Christina Varghese <46170618+christinichka@users.noreply.github.com>
2021-06-08 11:04:39 -0500

    Update looping-a-triangle.js
    Added a while loop example.

--- looping-a-triangle.js
+++ looping-a-triangle.js
@@ -1,3 +1,13 @@
+// Using a for loop
 for (let number = "#"; number.length < 8; number += "#") {
     console.log(number);
 }
+
+// Using a while loop
+let result = "";
+let number = 0;
+while (number < 7) {
+  result = result+"#";  
+  number = number + 1;
+  	console.log(result);
+            }
