9c65aa66a6131210f05c2e2e433036c456015062
Christina Varghese <46170618+christinichka@users.noreply.github.com>
2021-06-08 11:28:46 -0500

    Update fizzbuzz.js
    Adding a second solve for FizzBuzz.

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,7 +1,25 @@
 
+// One way to do FizzBuzz
 for (let i = 1; i <= 100; i++) {
     let number = "";
     if (i % 3 == 0) number += "Fizz";
     if (i % 5 == 0) number += "Buzz";
     console.log(number || i);
+}
+
+// A second way to do FizzBuzz
+for (let number = 1; number <= 100; number++) {
+  if (number % 3 == 0) {
+    console.log("Fizz"); 
+  }
+  else if (number % 5 == 0) {
+    console.log("Buzz");
+  }
+  else if (number % 5 == 0 && number == 3) {
+  	console.log("FizzBuzz");
+  }
+  else {
+  	console.log(number)
+  }
+  
 }
